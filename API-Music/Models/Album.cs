f158e4c3bccf460fd1491467165a33fe9d9b1187
FridayAI <gabi.get@hotmail.com>
2022-08-06 15:59:46 -0300

    Adiciona construtor Album

--- API-Music/Models/Album.cs
+++ API-Music/Models/Album.cs
@@ -8,7 +8,14 @@ namespace API_Music.Models
         public string Name { get; set; }
         public int YearLaunch { get; set; }
         public string CoverUrl { get; set; }
-        [Required(ErrorMessage = "Artista é obrigatório")]
         public Artist Artist { get; set; }
+
+        public Album(string name, int yearLaunch, string coverUrl, Artist artist)
+        {
+            Name = name;
+            YearLaunch = yearLaunch;
+            CoverUrl = coverUrl;
+            Artist = artist;    
+        }
     }
 }
