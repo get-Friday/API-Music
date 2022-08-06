84c0ba36ac1c1405226cee4b629d0360ed787dd4
FridayAI <gabi.get@hotmail.com>
2022-08-06 14:50:53 -0300

    Cria repositório de album

---
+++ API-Music/Repositories/AlbumRepository.cs
@@ -0,0 +1,36 @@
+﻿using API_Music.Models;
+
+namespace API_Music.Api.Repositories
+{
+    public class AlbumRepository
+    {
+        private static int indexId = 1;
+        private static readonly List<Album> _albums = new();
+        public Album Create(Album album)
+        {
+            album.Id = indexId++;
+            _albums.Add(album);
+
+            return album;
+        }
+        public void Delete(int id)
+        {
+            Album existingAlbum = _albums
+                .FirstOrDefault(a => a.Id == id);
+
+            _albums.Remove(existingAlbum);
+        }
+        public Album Update(int id, Album album)
+        {
+            Album existingAlbum = _albums
+                .FirstOrDefault(a => a.Id == id);
+
+            existingAlbum.Artist = album.Artist;
+            existingAlbum.CoverUrl = album.CoverUrl;
+            existingAlbum.YearLaunch = album.YearLaunch;
+            existingAlbum.Name = album.Name;
+
+            return album;
+        }
+    }
+}
