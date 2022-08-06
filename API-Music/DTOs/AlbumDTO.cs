c43351752f903277c5aaf176d3d4719933e8c5e7
FridayAI <gabi.get@hotmail.com>
2022-08-06 15:59:02 -0300

    Cria DTO

---
+++ API-Music/DTOs/AlbumDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.DTOs
+{
+    public class AlbumDTO
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Name { get; set; }
+        [Range(1500, 3000, ErrorMessage = "Ano de lançamento precisa ser válido.")]
+        public int YearLaunch { get; set; }
+        public string CoverUrl { get; set; }
+        [Required(ErrorMessage = "O artista é obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "O ID do artista precisa ser válido")]
+        public int ArtistId { get; set; }
+    }
+}
