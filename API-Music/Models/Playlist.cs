90542c953355f02065a67d5d14ae9fcb365d9564
FridayAI <gabi.get@hotmail.com>
2022-08-10 15:39:50 -0300

    Cria classe Playlist

---
+++ API-Music/Models/Playlist.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.Models
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public string Name { get; set; }
+        public List<Music> Musics { get; set; }
+    }
+}
