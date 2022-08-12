5cae6e6f609d462302e0dc388a247fc623020bd9
FridayAI <gabi.get@hotmail.com>
2022-08-12 20:20:26 -0300

    Cria viewmodel de retorno com falha

---
+++ API-Music/ViewModels/FailedReturn.cs
@@ -0,0 +1,11 @@
+﻿namespace API_Music.ViewModels
+{
+    public class FailedReturnViewModel
+    {
+        public string Failure { get; set; }
+        public FailedReturnViewModel(string failure)
+        {
+            Failure = failure;
+        }
+    }
+}
