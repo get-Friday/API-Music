3651f57738dd1601ec768e99e2fbe9015a282886
FridayAI <gabi.get@hotmail.com>
2022-08-12 20:10:50 -0300

    Cria DTO de artista

---
+++ API-Music/DTOs/ArtistDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.DTOs
+{
+    public class ArtistDTO
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [MaxLength(60)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Alias { get; set; }
+        public string PhotoUrl { get; set; }
+        public string CountryFrom { get; set; }
+    }
+}
