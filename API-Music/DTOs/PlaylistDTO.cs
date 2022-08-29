ebef473e2534071f226ec808656239400b516e66
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-29 18:12:43 -0300

    Feature: DTO para playlist

---
+++ API-Music/DTOs/PlaylistDTO.cs
@@ -0,0 +1,12 @@
+﻿using API_Music.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.DTOs
+{
+    public class PlaylistDTO
+    {
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public string Name { get; set; }
+        public List<Music> Musics { get; set; }
+    }
+}
