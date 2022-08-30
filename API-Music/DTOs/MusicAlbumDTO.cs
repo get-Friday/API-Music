2330f3e8eebaeda1158f9c7f4dfaf3790a6b052b
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-30 16:47:26 -0300

    Feature: DTO das musicas que vão no endpoint post album

---
+++ API-Music/DTOs/MusicAlbumDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.DTOs
+{
+    public class MusicAlbumDTO
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Name { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
