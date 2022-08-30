a66f53b6c2cacf1fe6731da32825a93313e06781
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-30 16:38:21 -0300

    Feature: Implementa DTO de album para edição apenas

---
+++ API-Music/DTOs/EditAlbumDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Music.DTOs
+{
+    public class EditAlbumDTO
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public string Name { get; set; }
+        [Range(1500, 3000, ErrorMessage = "Ano de lançamento precisa ser válido.")]
+        public int YearLaunch { get; set; }
+        public string CoverUrl { get; set; }
+    }
+}
