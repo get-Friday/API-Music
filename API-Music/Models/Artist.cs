0886a0ecd25ca5e35f9f6ff9e7c407bd0c655113
Gabriel "Friday" Thomas <gabriel.getdev@gmail.com>
2022-08-30 16:30:44 -0300

    Fix: Get album/musics para privado para não passar no objeto do controller

--- API-Music/Models/Artist.cs
+++ API-Music/Models/Artist.cs
@@ -11,7 +11,7 @@ namespace API_Music.Models
         public string PhotoUrl { get; set; }
         public string CountryFrom { get; set; }
 
-        public virtual List<Album> Albums { get; set; }
-        public virtual List<Music> Musics { get; set; }
+        public virtual List<Album> Albums { internal get; set; }
+        public virtual List<Music> Musics { internal get; set; }
     }
 }
